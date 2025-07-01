fc021add8a874e368d373d46c6e0f0a0a4c562b4
Jim Lin <jim@andestech.com>
2025-07-01 21:49:50 +0800

    Remove +zfh and +experimental-zvfh from LLVM header for float
    It doesn't need zfh and the intrinsics for zvfh have been splited out.

--- rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
@@ -469,8 +469,7 @@ class APITestGenerator(Generator):
 
 """
     float_llvm_header = r"""// REQUIRES: riscv-registered-target
-// RUN: %clang_cc1 -triple riscv64 -target-feature +v -target-feature +zfh \
-// RUN:   -target-feature +experimental-zvfh -disable-O0-optnone \
+// RUN: %clang_cc1 -triple riscv64 -target-feature +v -disable-O0-optnone \
 // RUN:   -emit-llvm %s -o - | opt -S -passes=mem2reg | \
 // RUN:   FileCheck --check-prefix=CHECK-RV64 %s
 
