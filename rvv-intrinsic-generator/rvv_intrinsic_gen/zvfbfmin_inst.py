3ffee4135b9903cf834f4640ffb13484df5eb60d
Jim Lin <jim@andestech.com>
2025-07-01 09:11:51 +0800

    Rename BFloat16 to zvfbfmin and remove zvfbfwma from LLVM header

--- rvv-intrinsic-generator/rvv_intrinsic_gen/zvfbfmin_inst.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/zvfbfmin_inst.py
@@ -41,8 +41,7 @@ TYPES = ["bfloat"]
 
 llvm_header = r"""// REQUIRES: riscv-registered-target
 // RUN: %clang_cc1 -triple riscv64 -target-feature +v \
-// RUN:   -target-feature +zvfbfmin \
-// RUN:   -target-feature +zvfbfwma -disable-O0-optnone \
+// RUN:   -target-feature +zvfbfmin -disable-O0-optnone \
 // RUN:   -emit-llvm %s -o - | opt -S -passes=mem2reg | \
 // RUN:   FileCheck --check-prefix=CHECK-RV64 %s
 
